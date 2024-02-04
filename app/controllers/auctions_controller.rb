a68bbfe2e07533e0f42d41257b3cda6665a89fd1
tylersalvato <tyler.harrissalvato@sdgku.edu>
2024-02-04 08:28:35 -0800

    edit and delete appear clickable but not functional

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,5 @@
 class AuctionsController < ApplicationController
-  skip_before_action  :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   before_action :set_auction, only: [:show, :update, :destroy]
 
   def index
@@ -12,36 +12,42 @@ class AuctionsController < ApplicationController
   end
 
   def create
-    @auction = Auction.new(auction_params)
-  
-    if @auction.save
-      render json: @auction, status: :created
-    else
-      render json: { error: 'Failed to create auction. Please try again.' }, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      @auction = Auction.new(auction_params)
+
+      if @auction.save
+        render json: @auction, status: :created
+      else
+        render json: { error: 'Failed to create auction. Please try again.' }, status: :unprocessable_entity
+      end
     end
   end
-  
 
   def update
-    if @auction.update(auction_params)
-      render json: @auction
+    if @auction
+      if @auction.update(auction_params)
+        render json: @auction
+      else
+        render json: @auction.errors, status: :unprocessable_entity
+      end
     else
-      render json: @auction.errors, status: :unprocessable_entity
+      render json: { error: 'Auction not found' }, status: :not_found
     end
   end
 
   def destroy
-    @auction.destroy
-    head :no_content
+    if @auction
+      @auction.destroy
+      head :no_content
+    else
+      render json: { error: 'Auction not found' }, status: :not_found
+    end
   end
 
   private
 
   def set_auction
     @auction = Auction.find_by(id: params[:id])
-    unless @auction
-      render json: { error: 'Auction not found' }, status: :not_found
-    end
   end
 
   def auction_params
